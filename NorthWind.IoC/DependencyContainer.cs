5ac12d21b9b8b80e9415149c3f7bc593c69e3d45
Usuario <julian19guerra@gmail.com@DESKTOP-FM3NDOL>
2024-12-15 18:02:02 -0500

    Agregar archivos de proyecto.

---
+++ NorthWind.IoC/DependencyContainer.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NorthWind.Core;
+using NorthWind.Entities.Interfaces;
+using NorthWind.Writers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.IoC
+{
+    public static class DependencyContainer
+    {
+        public static IServiceCollection AddNorthWindServices(this IServiceCollection services)
+        {
+            services.AddDebugWriter();
+            services.AddConsoleWriter();
+            services.AddFileWriter();
+            services.AddServices();
+            return services;
+        }
+    }
+}
---
+++ NorthWind.Writers/FileWriter.cs
@@ -0,0 +1,14 @@
+﻿using NorthWind.Entities.Interfaces;
+using NorthWind.Entities.ValueObjects;
+
+namespace NorthWind.Writers
+{
+    internal class FileWriter : IUserActionWriter
+    {
+        public void Write(UserAction action)
+        {
+            File.AppendAllText("AppLogs.txt",
+                string.Format("FileWriter: {0}, {1}, {2}", action.CreatedDateTime, action.User, action.Description));
+        }
+    }
+}
