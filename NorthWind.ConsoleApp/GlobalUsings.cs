67f2ac5339e78569a0fb1d1ad0e16d8a551e0ce2
Usuario <julian19guerra@gmail.com@DESKTOP-FM3NDOL>
2024-12-15 18:02:45 -0500

    Revert "Agregar archivos de proyecto."
    This reverts commit 5ac12d21b9b8b80e9415149c3f7bc593c69e3d45.

--- NorthWind.ConsoleApp/GlobalUsings.cs
+++
@@ -1,2 +0,0 @@
-global using NorthWind.Entities.Interfaces;
-global using NorthWind.Writers;
--- NorthWind.Entities/Interfaces/IUserActionWriter.cs
+++
@@ -1,9 +0,0 @@
-﻿using NorthWind.Entities.ValueObjects;
-
-namespace NorthWind.Entities.Interfaces
-{
-    public interface IUserActionWriter
-    {
-        void Write(UserAction action);
-    }
-}
